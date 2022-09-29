414829ce82f393ba374cbb41d6926ec350327b91
alvalenda <banysan@msn.com>
2022-09-29 10:21:53 -0300

    feat: useReducer > GithubReducer

--- src/contexts/github/GithubContext.jsx
+++ src/contexts/github/GithubContext.jsx
@@ -1,4 +1,5 @@
-import { createContext, useState } from "react";
+import { createContext, useReducer } from "react";
+import { githubReducer } from "./GithubReducer";
 
 const GithubContext = createContext();
 
@@ -6,8 +7,12 @@ const GITHUB_URL = import.meta.env.VITE_GITHUB_URL;
 const GITHUB_TOKEN = import.meta.env.VITE_GITHUB_TOKEN;
 
 export const GithubProvider = ({ children }) => {
-  const [users, setUsers] = useState(() => []);
-  const [isloading, setIsloading] = useState(() => true);
+  const initialState = {
+    users: [],
+    isLoading: true,
+  };
+
+  const [state, dispatch] = useReducer(githubReducer, initialState);
 
   const fetchUsers = async () => {
     const response = await fetch(`${GITHUB_URL}/users`, {
@@ -17,15 +22,17 @@ export const GithubProvider = ({ children }) => {
     });
 
     const data = await response.json();
-    setUsers(() => data);
-    setIsloading(() => false);
+
+    dispatch({
+      type: "GET_USERS",
+      payload: data,
+    });
   };
 
   return (
     <GithubContext.Provider
       value={{
-        users,
-        isloading,
+        ...state,
         fetchUsers,
       }}
     >
