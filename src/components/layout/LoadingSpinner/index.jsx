efeb04fe15cbdf95808b855a374edaccb02fa67d
alvalenda <banysan@msn.com>
2022-09-29 07:29:31 -0300

    feat: LoadingSpinner component

---
+++ src/components/layout/LoadingSpinner/index.jsx
@@ -0,0 +1,9 @@
+import './LoadingSpinner.css'
+
+export const LoadingSpinner = () => {
+  return (
+    <div className='spinner-container'>
+      <div className='loading-spinner'></div>
+    </div>
+  )
+}
