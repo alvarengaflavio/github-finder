ab395f15fc74d07d9a507efc2dec76ad8b91ec0b
alvalenda <banysan@msn.com>
2022-09-29 10:22:20 -0300

    fix: isLoading state

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -4,13 +4,13 @@ import { LoadingSpinner } from "../layout/LoadingSpinner";
 import { UserItem } from "./UserItem";
 
 export const UserResults = () => {
-  const { users, isloading, fetchUsers } = useContext(GithubContext);
+  const { users, isLoading, fetchUsers } = useContext(GithubContext);
 
   useEffect(() => {
     fetchUsers();
   }, []);
 
-  if (isloading) return <LoadingSpinner />;
+  if (isLoading) return <LoadingSpinner />;
 
   return (
     <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
