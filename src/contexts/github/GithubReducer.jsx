727417da76c2cd611ad9ab4cb23fbd2b9581f4d4
alvalenda <banysan@msn.com>
2022-09-29 10:21:31 -0300

    feat: GithubReducer

---
+++ src/contexts/github/GithubReducer.jsx
@@ -0,0 +1,13 @@
+export const githubReducer = (state, action) => {
+  switch (action.type) {
+    case "GET_USERS":
+      return {
+        ...state,
+        users: action.payload,
+        isLoading: false,
+      };
+
+    default:
+      return state;
+  }
+};
