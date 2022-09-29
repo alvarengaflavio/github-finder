5afe4dddbd6689239f4f059fee5939ca1ef4ac26
alvalenda <banysan@msn.com>
2022-09-29 07:29:52 -0300

    feat: UserResults component

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,6 +1,9 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { LoadingSpinner } from "../layout/LoadingSpinner";
 
 export const UserResults = () => {
+  const [users, setUsers] = useState(() => []);
+  const [isloading, setIsloading] = useState(() => true);
   useEffect(() => {
     fetchUsers();
   }, []);
@@ -13,8 +16,17 @@ export const UserResults = () => {
     });
 
     const data = await response.json();
-    console.log(data);
+    setUsers(() => data);
+    setIsloading(() => false);
   };
 
-  return <div>UserResults</div>;
+  if (isloading) return <LoadingSpinner />;
+
+  return (
+    <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
+      {users.map((user, i) => (
+        <h3 key={i}>{user.login}</h3>
+      ))}
+    </div>
+  );
 };
